803b5d13c4470ffff212780b21ba5e93d2a6131f
Aleksandar_Radanovic <a.radanovic1984@gmail.com>
2018-08-14 18:53:59 +0200

    zadatak 1

---
+++ src/components/AppCars.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>CARS</h1>
+</template>
+<script>
+export default {
+    name:'AppCars'
+}
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="navbar-nav">
+      <router-link class="nav-item nav-link" to="/cars">Cars</router-link>
+    </div>
+  </nav>
+</template>
+ <script>
+export default {
+    name: 'NavBar'
+}
+</script> 
